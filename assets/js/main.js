b26fb1354935701782510e4d7ec471db84a50ec4
BastienDeclaye <bastiendeclayepro@gmail.com>
2017-05-09 14:13:02 +0200

    pontoon + meadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,9 @@ Game.width = 1040;
 Game.height = 680;
 
 var Materials = {};
+var level = [];
 
-function Trunk(over, environment){
+function Trunk(environment, over = undefined){
     this.over = over;
     this.environment = environment;
     
@@ -18,15 +19,25 @@ function Vector(x, y = x){
 }
 
 function presetMaterial(key, cacheKey, tileSize, index){
+    //Create the `tileset` sprite from the texture
     Materials[key] = new PIXI.Texture(PIXI.BaseTexture.fromImage(cacheKey));
+    //Create a rectangle object that defines the position and tell the texture to use that rectangular section
     Materials[key].frame = new PIXI.Rectangle(tileSize.x * index.x, tileSize.y * index.y, tileSize.x, tileSize.y);
     console.log(Materials);
 }
 
+function levelEdit (start, end, trunk)
+{
+    for (var i = start; i < end; i++) 
+    {
+        level[i] = trunk;
+    }
+}
 
 Game.init = function(){
 
 	Game.stage = new PIXI.Container();
+        Game.stage.scale.x = Game.stage.scale.y = 2;
 	Game.renderer = PIXI.autoDetectRenderer(Game.width, Game.height);
 	document.body.appendChild(Game.renderer.view);
 	Game.preload();
@@ -58,25 +69,47 @@ Game.create = function(){
         presetMaterial("grass","assets/graphics/tiles.png", new Vector(32), new Vector(8, 0));
         presetMaterial("stone","assets/graphics/tiles.png", new Vector(32), new Vector(12, 0));
 
-	//Create the `tileset` sprite from the texture
-	 //var texture = PIXI.utils.TextureCache["assets/graphics/tiles.png"];
-
-	 //Create a rectangle object that defines the position and
-	 //size of the sub-image you want to extract from the texture
-
-	 //Tell the texture to use that rectangular section
-	 //texture.frame = new PIXI.Rectangle(256, 0, 32, 32);
-
+        level = new Array(300);
+        
+        let start = 0, end = level.length, random = Math.random() * 2;
+        if(Math.floor(random)) // 2 pontons
+        {
+            levelEdit(4, 12, new Trunk ("pontoon"));
+            start += 12;
+            levelEdit(end-12, end-4, new Trunk ("pontoon"));
+            end -= 12;
+        }
+        else if(random > 0.5)// 1 ponton à droite
+        {
+            levelEdit(end-12, end-4, new Trunk ("pontoon"));
+            end -= 12;
+        }
+        else // 1 ponton à gauche
+        {
+            levelEdit(4, 12, new Trunk ("pontoon"));
+            start += 12;
+        }
+        
+        levelEdit(start, end, new Trunk("meadow"));
+        
+        
+        
+        console.log(level);
+        
+       
+        
+        
+        
 	 //Create the sprite from the texture
 	var grass = new PIXI.Sprite(Materials["grass"]);
 	
 	 //Position the rocket sprite on the canvas
 	 grass.x = 64;
-	 grass.y = 400;
+	 grass.y = 200;
          var stone = new PIXI.Sprite(Materials["stone"]);
 
-	 stone.x = 128;
-	 stone.y = 400;
+	 stone.x = 96;
+	 stone.y = 200;
 
 	//Add the rocket to the stage
 	Game.stage.addChild(grass);
